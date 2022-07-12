05e527971920f36f81682267939df258cad73a3a
Juan Bohorquez Marcelo <juan.bohorquezm@perficient.com>
2022-07-12 12:47:27 -0500

    Step 8 CSS Selectors done

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,8 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   video: false,
   screenshotOnRunFailure: false,
+  "defaultCommandTimeout": 20000,
+    "responseTimeout": 20000,
 
   e2e: {
     setupNodeEvents(on, config) {
