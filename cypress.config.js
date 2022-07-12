b9519c1cfbce9884d04d04272868144d77a3c17a
JuanBohorquezPerficient <107504371+JuanBohorquezPerficient@users.noreply.github.com>
2022-07-12 14:10:14 -0500

    Setup (#4)
    * setup project configuration
    
    * eof fix
    
    * 4th step done, gonna install cypress 10
    
    * setup cypress configuration
    
    * gonna make checkout to pull from origin main to local main
    
    * preparing to initialize 8 CSS selectors
    
    * nvm file fixed
    
    * fixing issues
    
    * Step 8 CSS Selectors done
    
    * indent fixed
    
    * problem related to lint popped up, trying to fix it with an additional step on yml file before npm ci
    
    * gonna delete examples

--- cypress.config.js
+++ cypress.config.js
@@ -4,6 +4,10 @@ module.exports = defineConfig({
   video: false,
   screenshotOnRunFailure: false,
 
+  "defaultCommandTimeout": 20000,
+  "responseTimeout": 20000,
+
+
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
