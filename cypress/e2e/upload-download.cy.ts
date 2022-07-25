f77e343679b08f09362ed86f52b735e2ad2e9d51
JuanBohorquezPerficient <107504371+JuanBohorquezPerficient@users.noreply.github.com>
2022-07-25 11:35:10 -0500

    Setup (#11)
    * setup project configuration
    
    * eof fix
    
    * 4th step done, gonna install cypress 10
    
    * setup cypress configuration
    
    * gonna make checkout to pull from origin main to local main
    
    * preparing to initialize 8 CSS selectors
    
    * nvm file fixed
    
    * fixing issues
    
    * Step 8 CSS Selectors done
    
    * indent fixed
    
    * problem related to lint popped up, trying to fix it with an additional step on yml file before npm ci
    
    * gonna delete examples
    
    * Step 9 Page Object Model already done and tested
    
    * fixing issues found by manuel
    
    * another fixing
    
    * Triple A pattern implemented
    
    * trim fixed
    
    * Elements list, filter and elements inside elements chapter done
    
    * Improving reports - Mochawesome chapter done, awaiting for comments about hooks if there are some comments
    
    * Filling form step done, preparing to start step 15 uploading a file
    
    * changing button.click() for submit()
    
    * task done about creating an external interface and added new packages models and e2e/questions for assertions
    
    * fixing type issues
    
    * some changes
    
    * tests passed locally but not in CI
    
    * blank spaces which appeared after resolvin conflicts were deleted
    
    * I've made chenges about the comments
    
    * added fixes related to reviewers comments
    
    * pic file address encoded base 64... I'll search how to show it as normal address
    
    * step 16 downloading a file completed
    
    * Erased all lines in which Holmes gave the advice to erase, preparing all the project to fulfill the last step of this workshop
    
    * parts of the next step deleted

--- cypress/e2e/upload-download.cy.ts
+++ cypress/e2e/upload-download.cy.ts
@@ -1,10 +1,15 @@
 import {Upload} from "../page/index";
 
+import {DownloadPage} from "../page/index";
+
 const upload: Upload = new Upload();
+const download: DownloadPage = new DownloadPage();
 
 describe("Upload and download a file", ()=>{
   const targetFile = "textForTest.txt";
-  const expectedText = "textForTest";
+  const expectedUploadText = "textForTest";
+  const expectedFileName = "sampleFile.jpeg";
+
   before(()=>{
     cy.fixture("upload");
   });
@@ -12,6 +17,12 @@ describe("Upload and download a file", ()=>{
   it("Should upload a file", ()=>{
     upload.visitTestPage();
     upload.uploadFile(targetFile);
-    upload.getTitle().should("contain.text", expectedText);
+    upload.getTitle().should("contain.text", expectedUploadText);
+  });
+
+  it("Should download a file", ()=>{
+    download.visitPage();
+    download.downloadFile();
+    download.verifyDownloadFileContent(expectedFileName);
   });
 });
